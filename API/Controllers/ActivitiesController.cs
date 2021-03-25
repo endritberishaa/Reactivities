93aa01fd0a2d27cc4e4d5f2c55223fa7283a687d
endritberishaa <endritpb@gmail.com>
2021-03-25 13:56:06 +0100

    End of Section 2

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseAPIController
+    {
+        private readonly DataContext context;
+        public ActivitiesController(DataContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities(){
+            return await this.context.Activities.ToListAsync();
+        }
+        [HttpGet("{id}")] //activities/id
+        public async Task<ActionResult<Activity>> GetActivity(Guid id) {
+            return await this.context.Activities.FindAsync(id);
+        }
+    }
+}
