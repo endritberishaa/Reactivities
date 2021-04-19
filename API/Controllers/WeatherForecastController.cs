0bc856dbe6b5473fdb9d839fb3c653c6ff9add4e
endritberishaa <endritpb@gmail.com>
2021-04-19 18:39:47 +0200

    End of Skeleton

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/*A controller is responsible for controlling the way that a user interacts with an MVC application.
+ A controller contains the flow control logic for an ASP.NET MVC application.
+ A controller determines what response to send back to a user when a user makes a browser request.*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
--- API/Startup.cs
+++ API/Startup.cs
@@ -27,8 +27,6 @@ namespace API
 
         }
 
-
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -41,6 +39,11 @@ namespace API
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>{
+                opt.AddPolicy("CorsPolicy", policy =>{
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
             });
         }
 
@@ -57,6 +60,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
