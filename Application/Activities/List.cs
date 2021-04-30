f92e534a52a0b637a176cd8db713e0c5c5eb378f
endritberishaa <endritpb@gmail.com>
2021-04-30 11:47:24 +0200

    End of Section 5

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -11,22 +11,20 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>> { }
+        public class Query : IRequest<List<Activity>> {}
 
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
-            private readonly ILogger<List> _logger;
-            public Handler(DataContext context, ILogger<List> logger)
+            public Handler(DataContext context)
             {
-                _logger = logger;
                 _context = context;
 
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.Activities.ToListAsync(cancellationToken);
+                return await _context.Activities.ToListAsync();
             }
         }
     }
